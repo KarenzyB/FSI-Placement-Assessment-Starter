5478e5589a01e199e84f04657c90e6f4046d5401
Karenzyyy <kborbore@gmail.com>
2022-01-25 21:10:19 -0500

    final draft

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,77 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
-
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+let yourName = "Karen Borbor"
 
 // selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+let credit = document.querySelector('#credit')
 
-// Code to update name display
-credit.textContent = `Created by ${yourName}`
+// We'll use these variables to track the counts of each cookie type 
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
+//create an array
+let cookies = [
+    gb = 0,
+    cc = 0,
+    sugar = 0
+]
+// let gb = 0 // Gingerbread
+// let cc = 0 // Chocolate Chip
+// let sugar = 0 // Sugar Sprinkle
+
+
+//gb button and totals
+
+let gbPlusBtn = document.querySelector('#add-gb')
+let gbMinusBtn = document.querySelector('#minus-gb')
+
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+    gb = gb + 1;
+    document.querySelector('#qty-total').textContent = gb + cc + sugar;
+    document.querySelector('#qty-gb').textContent = gb;
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+})
+gbMinusBtn.addEventListener('click', function() {
+        gb = gb - 1;
+        if (gb < 0) gb=0
+        document.querySelector('#qty-total').textContent = gb + cc + sugar;
+        document.querySelector('#qty-gb').textContent = gb;
+    
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+//cc buttons and totals
+let ccPlusBtn = document.querySelector('#add-cc')
+let ccMinusBtn = document.querySelector('#minus-cc')
+
+ccPlusBtn.addEventListener('click', function() {
+    cc = cc + 1;
+    document.querySelector('#qty-total').textContent = gb + cc + sugar;
+    document.querySelector('#qty-cc').textContent = cc;
+
+})
+ccMinusBtn.addEventListener('click', function() {
+    cc = cc - 1;
+    if (cc < 0) cc=0;
+    document.querySelector('#qty-total').textContent = gb + cc + sugar;
+    document.querySelector('#qty-cc').textContent = cc;
+
+})
+
+//sugar buttons and totals
+let sugarPlusBtn = document.querySelector('#add-sugar')
+let sugarMinusBtn = document.querySelector('#minus-sugar')
+
+sugarPlusBtn.addEventListener('click', function() {
+    sugar = sugar + 1;
+    document.querySelector('#qty-total').textContent = gb + cc + sugar;
+    document.querySelector('#qty-sugar').textContent = sugar;
+
+})
+sugarMinusBtn.addEventListener('click', function() {
+    sugar = sugar - 1;
+    if (sugar < 0) sugar = 0
+    document.querySelector('#qty-total').textContent = gb + cc + sugar;
+    document.querySelector('#qty-sugar').textContent = sugar;
+})
+
+// Code to update name display
+document.querySelector('#credit').textContent =  `Created by ${yourName}`;
+
+//document.querySelector('#qty-total').textContent = sum+=cookies;
+//for loop for sum?
